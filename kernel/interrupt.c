cb6b528c0d35b02ebce51ca91e4a931a6ef16cd2
xxcisxxc <32963789+xxcisxxc@users.noreply.github.com>
2023-04-04 23:40:26 -0400

    Add Assert

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -12,6 +12,10 @@
 
 #define IDT_DESC_CNT 0x21
 
+#define EFLAGS_IF 0x00000200
+#define GET_EFLAGS(EFLAG_VAR) asm volatile("pushfl; popl %0" : "=g" (EFLAG_VAR))
+
+
 struct gate_desc {
   uint16_t func_offset_low_word;
   uint16_t selector;
@@ -95,6 +99,40 @@ static void exception_init(void) {
   intr_name[17] = "#AC Alignment Check Exception";
   intr_name[18] = "#MC Machine-Check Exception";
   intr_name[19] = "#XF SIMD Floating-Point Exception";
+}
+
+enum intr_status intr_enable() {
+   enum intr_status old_status;
+   if (INTR_ON == intr_get_status()) {
+      old_status = INTR_ON;
+      return old_status;
+   } else {
+      old_status = INTR_OFF;
+      asm volatile("sti");
+      return old_status;
+   }
+}
+
+enum intr_status intr_disable() {     
+   enum intr_status old_status;
+   if (INTR_ON == intr_get_status()) {
+      old_status = INTR_ON;
+      asm volatile("cli" : : : "memory"); 
+      return old_status;
+   } else {
+      old_status = INTR_OFF;
+      return old_status;
+   }
+}
+
+enum intr_status intr_set_status(enum intr_status status) {
+   return status & INTR_ON ? intr_enable() : intr_disable();
+}
+
+enum intr_status intr_get_status() {
+   uint32_t eflags = 0; 
+   GET_EFLAGS(eflags);
+   return (EFLAGS_IF & eflags) ? INTR_ON : INTR_OFF;
 }
 
 void idt_init() {
