a0f9523dd02b8b3ec62b98145cb6f96067192bce
xxcisxxc <32963789+xxcisxxc@users.noreply.github.com>
2023-09-30 22:22:06 -0500

    try to alloc memory

--- kernel/main.c
+++ kernel/main.c
@@ -1,11 +1,19 @@
-#include "print.h"
 #include "init.h"
+#include "memory.h"
+#include "print.h"
+#include <stdint.h>
 
 int main(void) {
   put_str("I am Kernel\n");
   init_all();
-  asm volatile("sti");
-  put_str("Hello\n");
-  while(1);
+  // asm volatile("sti");
+
+  void *addr = get_kernel_pages(3);
+  put_str("\nget_kernel_page start vaddr is ");
+  put_int((uint32_t)addr);
+  put_str("\n");
+
+  while (1)
+    ;
   return 0;
 }
